f3c92055d480e860a037eaa975c86eb7258bc787
Cecilia Nilsson <cecilia.o.nilsson@live.se>
2020-12-10 13:13:10 +0100

    Added list of members and method for members-list in console.

--- Inlamningsuppgift02/Program.cs
+++ Inlamningsuppgift02/Program.cs
@@ -1,26 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlamningsuppgift02
 {
-    //Class Member
-    //private string name;
-    //private int height;
-    //private int age;
-    //private string hobbies;
-    //private string food;
-    //private string color;
-    //private string motivation;
-    //private string home;
-    //private string birthplace;
-    //private int siblings;
+    class Member
+    {
+        private string name;
+        private int age;
+        private int height;
+        private string home;
+        private string birthplace;
+        private int siblings;
+        private string color;
+        private string food;
+        private string hobbies;
+        private string motivation;
+
+        public Member (string name, int age, int height, string home, string birthplace, int siblings, string color, string food, string hobbies, string motivation)
+            {
+            this.Name = name;
+            this.Age = age;
+            this.Height = height;
+            this.Home = home;
+            this.Birthplace = birthplace;
+            this.Siblings = siblings;
+            this.Color = color;
+            this.Food = food;
+            this.Hobbies = hobbies;
+            this.Motivation = motivation;
+            }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public int Height { get => height; set => height = value; }
+        public string Home { get => home; set => home = value; }
+        public string Birthplace { get => birthplace; set => birthplace = value; }
+        public int Siblings { get => siblings; set => siblings = value; }
+        public string Color { get => color; set => color = value; }
+        public string Food { get => food; set => food = value; }
+        public string Hobbies { get => hobbies; set => hobbies = value; }
+        public string Motivation { get => motivation; set => motivation = value; }
+
+        public override string ToString()
+        {
+            return $"{name}, " +
+                $"Ålder: {age}, " +
+                $"Längd: {height}, " +
+                $"Hemort: {home}, " +
+                $"Födelseort: {birthplace}, " +
+                $"Antal syskon: {siblings}, " +
+                $"Favoritfärg: {color}, " +
+                $"Favoritmat: {food}, " +
+                $"Hobby: {hobbies}, " +
+                $"Motivation till programmering: {motivation}";
+        }
+    }
+
     class Program
     {
+        static List<Member> bästkustenMembers = new List<Member>();
+
         static void Main(string[] args)
         {
-
+            
             Password();
+            MembersList();
             StartMenu();
-
 
         }
 
@@ -72,7 +117,7 @@ namespace Inlamningsuppgift02
                 switch (userChoice)
                 {
                     case "a":
-                        MembersList();
+                        ShowMembersList();
                         break;
 
                     case "b":
@@ -90,14 +135,43 @@ namespace Inlamningsuppgift02
                 }
             } while (run);
         }
-
-        //-MemberList-metod-
+        //lista av Member
         static void MembersList()
         {
+            Member Cecilia = new Member("Cecilia", 29, 163, "Norrköping", "Norrköping", 1, "Gul", "Risotto", "Spela The Sims", "Få arbeta kreativt");
+            Member David = new Member("David", 32, 183, "Norrtälje", "Göteborg", 1, "Blå", "Tacos", "BJJ", "Problemlösning");
+            Member Elin = new Member("Elin", 31, 170, "Knivsta", "Karlskoga", 2, "Röd", "Sushi", "Hästar", "Personlig utveckling");
+            Member Ivo = new Member("Ivo", 42, 174, "Uppsala", "Split", 1, "Svart", "Scampi", "Fotografi", "Kreativitet");
+            Member Jeremy = new Member("Jeremy", 19, 181, "Djurö", "Köln", 1, "Teal", "Älggryta", "Gaming", "Jobb");
+            Member Johan = new Member("Johan", 34, 194, "Mariefred", "Mariefred", 2, "Blå", "Tacos", "Gaming", "Trygg framtid");
+            Member Mostafa = new Member("Mostafa", 33, 174, "Stockholm", "Stockholm", 4, "Svart", "Oxfilé", "Träning", "Tycker om att koda och lösa problem");
+            Member Oscar = new Member("Oscar", 26, 185, "Stockholm", "Stockholm", 1, "Blå", "Lasagne", "Fotboll", "Jobb");
+            Member Rami = new Member("Rami", 0, 0, "Okänt", "Okänt", 0, "Okänt", "Okänt", "Okänt", "Okänt");
+            Member Sanjin = new Member("Sanjin", 30, 179, "Norrköping", "Mostar", 2, "Blå", "Pizza", "Fotboll", "Jobb");
+
+            bästkustenMembers.Add(Cecilia);
+            bästkustenMembers.Add(David);
+            bästkustenMembers.Add(Elin);
+            bästkustenMembers.Add(Ivo);
+            bästkustenMembers.Add(Jeremy);
+            bästkustenMembers.Add(Johan);
+            bästkustenMembers.Add(Mostafa);
+            bästkustenMembers.Add(Oscar);
+            bästkustenMembers.Add(Rami);
+            bästkustenMembers.Add(Sanjin);
+        }
+        //-MemberList-metod-
+        static void ShowMembersList()
+        {
             
-            //Skapa lista av human
-            //Gör en for each-loop som listar upp medlemar
+            //Gör en for-loop som listar upp medlemar
             Console.WriteLine("Bästkustens medlemmar: ");
+
+            for (int i = 0; i < bästkustenMembers.Count; i++)
+            {
+                Console.WriteLine(bästkustenMembers[i].Name);
+            }
+
             Console.WriteLine("---------------------------");
         }
 
@@ -115,7 +189,7 @@ namespace Inlamningsuppgift02
         static void RemoveMember()
         {
             
-            //Tar bort vald medlem från listan, använd en for-loop och be user om nummer från member-lista
+            //Tar bort vald medlem från listan, använd en for-loop och be user om namn från member-lista
             Console.WriteLine("Skriv namnet på den medlem som ska tas bort:");
             Console.Write("Namn:");
             Console.WriteLine("---------------------------");
